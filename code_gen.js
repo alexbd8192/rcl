799b38bd8933df7fea4d1fec68fc361e3dedb123
Alexandre Denommee <alex@alexandredenommee.com>
2024-05-08 14:08:23 -0400

    Ajout code a 6 chiffre.

--- code_gen.js
+++ code_gen.js
@@ -1,45 +1,94 @@
-       function maxLengthCheck(input) {
-            if (input.value.length > input.maxLength) {
-                input.value = input.value.slice(0, input.maxLength);
-            }
-        }
+function maxLengthCheck(input) {
+    if (input.value.length > input.maxLength) {
+        input.value = input.value.slice(0, input.maxLength);
+    }
+}
 
-        function generateStrings() {
-            var seed = document.getElementById('seed').value;
+function generateStrings() {
+    var seed = document.getElementById('bkmrk-seed').value;
 
-            // Vérifier si le seed est vide ou pas un chiffre
-            if (!seed || isNaN(seed)) {
-                alert('Please enter a valid seed.');
-                return;
-            }
+    // Vérifier si le seed est vide ou pas un chiffre
+    if (!seed || isNaN(seed)) {
+        alert('SVP Entrer un no de client valide.');
+        return;
+    }
 
-            // S'assure que c'est maximum 5 chiffres
-            seed = seed.slice(0, 5);
+    // S'assure que c'est maximum 5 chiffres
+    seed = seed.slice(0, 5);
 
-            // Génèere les différent codes
-            var string1 = generateNumericString(seed, 4);
-            var string4 = generateMixedString(seed, 6);
-            var string2 = generateMixedString(seed, 8);
-            var string3 = generateMixedString(seed, 16);
+    // Génère les différent codes, via id dans un tag html
+    var string1 = generate4NumericString(seed, 4);
+    var string2 = generateString(seed, 8);
+    var string3 = generateMixedString(seed, 16);
+    var string4 = generate8NumericString(seed, 8);
+    var string5 = generate8NumericString(seed, 6);
 
-            // Affiche les différents codes
-            document.getElementById('string1').innerText = "1) " + string1;
-            document.getElementById('string4').innerText = "2) " + string4;
-            document.getElementById('string2').innerText = "3) " + string2;
-            document.getElementById('string3').innerText = "4) " + string3;
-            
-        }
+    // Affiche les différents codes
+    document.getElementById('bkmrk-string1').innerText = string1;
+    document.getElementById('bkmrk-string2').innerText = string2;
+    document.getElementById('bkmrk-string3').innerText = string3;
+    document.getElementById('bkmrk-string4').innerText = string4;
+    document.getElementById('bkmrk-string5').innerText = string5;
+}
 
-        function generateNumericString(seed, length) {
-            seed = (seed * 31 + 17) % 9973;
-            return Math.abs(seed).toString().padStart(length, '0').slice(0, length);
-        }
+function generate4NumericString(seed, length) {
+    seed = (seed * 31 + 17) % 9973;   // Algorithme pseudo-random
 
-        function generateMixedString(seed, length) {
-            var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+[]{}|;:,.<>?';
-            var result = '';
-            for (var i = 0; i < length; i++) {
-                result += chars.charAt(Math.floor(seed % chars.length));
-                seed = (seed * 17 + 5) % 9973; // Algorythme pseud-random
-            }
-            return result;
+    return Math.abs(seed).toString().padStart(length, '0').slice(0, length);
+}
+
+function generate6NumericString(seed, length) {
+    seed = (seed * 31 + 17) % 9973;   // Algorithme pseudo-random
+    return Math.abs(seed).toString().padStart(length, '0').slice(0, length);
+}
+
+function generate8NumericString(seed, length) {
+    seed = (seed * 315560 + 17) / 20;   // Algorithme pseudo-random
+    seed = Math.round(seed);
+    return Math.abs(seed).toString().padStart(length, '0').slice(0, length);
+}
+
+function generateString(seed, length) {
+    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    var result = '';
+    for (var i = 0; i < length; i++) {
+        result += chars.charAt(Math.floor(seed % chars.length));
+        seed = (seed * 17 + 5) % 9973; // Algorithme pseudo-random
+    }
+    return result;
+}
+
+/*
+function generateMixedString(seed, length) {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+[]{}|;:,.<>?';
+    var result = '';
+    for (var i = 0; i < length; i++) {
+        result += chars.charAt(Math.floor(seed % chars.length));
+        seed = (seed * 17 + 5) % 9973; // Algorithme pseudo-random
+    }
+    return result;
+}
+*/
+
+function generateMixedString(seed, length) {
+    var chars_small = 'abcdefghijklmnopqrstuvwxyz';
+    var chars_caps = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var chars_num = '0123456789';
+    var chars_spec = '!@#$%^&*()_+[]{}|;:,.<>?';
+    var result = '';
+    for (var i = 0; i < length/4; i++) {
+        result += chars_small.charAt(Math.floor(seed % chars_small.length));
+        result += chars_caps.charAt(Math.floor(seed % chars_caps.length));
+        result += chars_num.charAt(Math.floor(seed % chars_num.length));
+        result += chars_spec.charAt(Math.floor(seed % chars_spec.length));
+        seed = (seed * 17 + 5) % 9973; // Algorithme pseudo-random
+    }
+    return result;
+}
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    const gen_button = document.getElementById("bkmrk-button");
+    if (gen_button) {
+        gen_button.addEventListener("click", generateStrings) ;
+    }
+});
